1c9e5d2545f2f3a983b1c0087fa73d4fb318deb1
MariaJose <mariajoserucla@gmail.com>
2025-07-17 09:20:26 +0200

    Cambios

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './component/header/header';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, FormsModule],
   standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  number: number = 0;
+  userName: string = 'María José';
 
-  addOne() {
-    this.number++
-  }
+
 }
